7d50ebcbba0826ccb59bccbb5cd711f340bc81d1
Ruici Luo <luoruici@gmail.com>
2016-06-17 16:11:48 +0800

    fix relative path.

--- src/ace.js
+++ src/ace.js
@@ -5,6 +5,6 @@ require("../ace/ext-language_tools");
 require("../ace/ext-whitespace");
 
 var aceconfig = ace.require("ace/config");
-aceconfig.set("basePath", "packages/editor/ace");
+aceconfig.set("basePath", "/packages/editor/ace");
 
 module.exports = window.ace;
